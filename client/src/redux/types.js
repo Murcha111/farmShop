b8f1fd855351252fa2d8e4fcab389bcc84b97239
Murcha111 <lenaev84@gmail.com>
2022-11-16 12:12:04 +0300

    client

---
+++ client/src/redux/types.js
@@ -0,0 +1,7 @@
+export const ADD_CARD = 'ADD_CARD'
+export const SET_CARDS = 'SET_CARDS'
+export const SET_ONE_CARD = 'SET_ONE_CARD'
+export const EDIT_CARD = 'EDIT_CARD'
+export const SET_INFO = 'SET_INFO'
+export const GET_CARDS = 'GET_CARDS'
+
